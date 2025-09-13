70b84fe79b340119ff94fcc0948477465f49eadd
Wojciech Klama <wojciechklamaczp@gmail.com>
2025-09-13 20:11:06 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -24,19 +24,34 @@
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
+    };
+
+    const addNewTask = (newTaskContent) =>{
+            tasks.push({
+                content: newTaskContent,
+            });
+
+            render();
+    };
+
+    const onFormSubmit = (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+            
+            if (newTaskContent === "") {
+                return;
+            }
+        
+            addNewTask(newTaskContent);
+    };
 
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            console.log(newTaskContent);
-        });
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
